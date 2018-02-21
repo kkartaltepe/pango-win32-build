2b5b8725b1ba81187e469a36b00130d7b9a0887a
Kurt Kartaltepe <kkartaltepe@gmail.com>
2018-02-20 18:19:10 -0600

    Build success!

--- prepare.sh
+++ prepare.sh
@@ -4,21 +4,19 @@
 mkdir -p dl
 wget -P dl -i download.list
 
-#Untar them all
+# #Untar them all
 mkdir -p srcs
 while read f; do
 	tar -xvf "$f" -C srcs
 done < <(find ./dl -type f)
 
-#clone cmake repos
+# #clone cmake repos
 mkdir -p "patches/cmake"
 cd "patches/cmake"
-while read f; do
-	git clone "$f"
-done < ../../cmakelists.list
+while read f; do git clone "$f"; done < ../../cmakelists.list
 cd ../../
 
-# Add cmake to sources
+# # Add cmake to sources
 while read cm; do
 	p=`basename ${cm%-*}`
 	rsync -a "$cm/" srcs/${p}*.*
@@ -28,69 +26,89 @@ done < <(find "patches/cmake/" -mindepth 1 -maxdepth 1 -name "*cmake")
 mkdir -p build
 while read p; do
 	p=`basename ${p%-*}`
-	mkdir "build/$p"
+	mkdir -p "build/$p"
 done < <(ls srcs)
 
-# setup pkgconfig
-export PKG_CONFIG_LIBDIR="$(cygpath --mixed `pwd`/install/share/pkgconfig)"
-
-# apply patches
+#glib ;_;
 cd srcs
-patch -p0 < ../patches/*.patch # apply all patches
+patch -p0 < ../patches/glib*.prepatch
+export PKG_CONFIG_LIBDIR='/' # stop it from using cygwin pkg-config
+#export PKG_CONFIG_LIBDIR="$(cygpath --mixed `pwd`/install/share/pkgconfig)" # Just let them build their internal zlib
+cd glib-*/
+meson ../../build/glib/ --prefix="`cygpath --mixed ../../install/`" --buildtype=debugoptimized
+cd ../
+cat ../patches/glib*.postpatch | patch -p0 # apply all patches
+cd ../build/glib
+ninja install # required before harfbuzz for pango
+cd ../../
+export PKG_CONFIG_LIBDIR="$(cygpath --unix `pwd`/install/lib/pkgconfig)" # needed for glib (TODO: PUT ME IN THE RIGHT PLACE)
+
+# apply general patches
+cd srcs
+cat ../patches/*.patch | patch -p0 # apply all patches
 cd ../
 
-exit 0; # end before cmakes
-# cmake_defaults="-G 'Ninja' -DCMAKE_BUILD_TYPE=RelWithDebInfo -DCMAKE_INSTALL_PREFIX=../../install"
 #zlib
-cmake ../../srcs/zlib-1.2.11/ \
+cd build/zlib
+cmake ../../srcs/zlib-*/ \
     -G 'Ninja' -DCMAKE_BUILD_TYPE=RelWithDebInfo -DCMAKE_INSTALL_PREFIX=../../install
+ninja install
 
 #libpng
-cmake ../../srcs/libpng-1.6.34/ \
+cd ../libpng
+cmake ../../srcs/libpng-*/ \
     -G 'Ninja' -DCMAKE_BUILD_TYPE=RelWithDebInfo -DCMAKE_INSTALL_PREFIX=../../install
+ninja install
 
 #freetype no hb
-cmake ../../srcs/freetype-2.9/ \
-    -DWITH_PNG=ON
+cd ../freetype
+cmake ../../srcs/freetype-*/ \
+    -DWITH_PNG=ON \
     -G 'Ninja' -DCMAKE_BUILD_TYPE=RelWithDebInfo -DCMAKE_INSTALL_PREFIX=../../install
+ninja install
 
 #harfbuzz (hb-glib required by pango fontconfig/freetype support.)
-cmake ../../srcs/harfbuzz-1.7.5/ \
+cd ../harfbuzz
+cmake ../../srcs/harfbuzz-*/ \
     -DHB_HAVE_FREETYPE=ON -DHB_HAVE_GLIB=ON \
     -G 'Ninja' -DCMAKE_BUILD_TYPE=RelWithDebInfo -DCMAKE_INSTALL_PREFIX=../../install
+ninja install
 
 #freetype with hb
-cmake ../../srcs/freetype-2.9/ \
+cd ../freetype
+cmake ../../srcs/freetype-*/ \
     -DWITH_PNG=ON -DWITH_HARFBUZZ=ON \
     -G 'Ninja' -DCMAKE_BUILD_TYPE=RelWithDebInfo -DCMAKE_INSTALL_PREFIX=../../install
+ninja install
 
 #libexpat
-cmake ../../srcs/libexpat-R_2_2_5/expat/ \
+cd ../libexpat
+cmake ../../srcs/libexpat-*/expat/ \
     -DBUILD_examples=off -DBUILD_tests=off -DBUILD_shared=OFF \
     -G 'Ninja' -DCMAKE_BUILD_TYPE=RelWithDebInfo -DCMAKE_INSTALL_PREFIX=../../install
+ninja install
 
 #fontconfig
-cmake ../../srcs/fontconfig-2.12.6/ \
+cd ../fontconfig
+cmake ../../srcs/fontconfig-*/ \
     -G 'Ninja' -DCMAKE_BUILD_TYPE=RelWithDebInfo -DCMAKE_INSTALL_PREFIX=../../install
+ninja install
+#ConfigureChecks.cmake, Config.h.cmake
 
 #pixman
-cmake ../../srcs/pixman-0.34.0/ \
+cd ../pixman
+cmake ../../srcs/pixman-*/ \
     -G 'Ninja' -DCMAKE_BUILD_TYPE=RelWithDebInfo -DCMAKE_INSTALL_PREFIX=../../install
-
-#glib ;_;
-export PKG_CONFIG_LIBDIR='/' # stop it from using cygwin pkg-config
-cd srcs/glib-2.55.2/
-meson ../../build/glib/ --prefix="`cygpath --mixed ../../install/`" --buildtype=debugoptimized
-cd ../../build/glib
-ninja
-# PCRE fix, libintl static, libffi static (stop using OBJECT:!!!!)
-# Patch unistd out (not needed in windows...) in gio\gwin32networkmonitor.c
-export PKG_CONFIG_LIBDIR="$(cygpath --mixed `pwd`/install/share/pkgconfig)"
+ninja install
 
 #cairo
-cmake ../../srcs/cairo-1.15.10/ \
+cd ../cairo
+cmake ../../srcs/cairo-*/ \
     -G 'Ninja' -DCMAKE_BUILD_TYPE=RelWithDebInfo -DCMAKE_INSTALL_PREFIX=../../install
+ninja install
 
 #pango
-cmake ../../srcs/pango-1.41.0 \
+cd ../pango
+cmake ../../srcs/pango-* \
     -G 'Ninja' -DCMAKE_BUILD_TYPE=RelWithDebInfo -DCMAKE_INSTALL_PREFIX=../../install
+ninja install
